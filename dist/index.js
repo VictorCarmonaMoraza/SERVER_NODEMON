49076285ab3a78d73e66179e2898e5113080254c
Victor <VictorCarmona9035@hotmail.com>
2023-11-21 08:01:11 +0100

    Creacion servidor nodemon

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+// const server = new Server();
+// server.start(()=>{
+//     console.log('Servidor corriendo');
+// })
---
+++ index.ts
@@ -0,0 +1,8 @@
+
+
+
+// const server = new Server();
+
+// server.start(()=>{
+//     console.log('Servidor corriendo');
+// })
